d69c6623db654b5f847732ac4715b9410b58446b
JaeWoong Eum <wodnd0418@gmail.com>
2026-06-09 20:15:41 +0900

    chore: Project.swift 초기 세팅

--- Project.swift
+++ Project.swift
@@ -2,12 +2,39 @@ import ProjectDescription
 
 let project = Project(
     name: "Bookmark",
+    settings: .settings(
+        base: [
+            "SWIFT_VERSION": "6.0",
+            "IPHONEOS_DEPLOYMENT_TARGET": "17.0"
+        ]
+    ),
     targets: [
         .target(
-            name: "Bookmark",
+            name: "Bookmark-Dev",
             destinations: .iOS,
             product: .app,
-            bundleId: "dev.tuist.Bookmark",
+            bundleId: "com.wodnd.bookmark",
+            deploymentTargets: .iOS("17.0"),
+            infoPlist: .extendingDefault(
+                with: [
+                    "UILaunchScreen": [
+                        "UIColorName": "",
+                        "UIImageName": "",
+                    ],
+                ]
+            ),
+            buildableFolders: [
+                "Bookmark/Sources",
+                "Bookmark/Resources",
+            ],
+            dependencies: []
+        ),
+        .target(
+            name: "Bookmark-Live",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "com.wodnd.bookmark",
+            deploymentTargets: .iOS("17.0"),
             infoPlist: .extendingDefault(
                 with: [
                     "UILaunchScreen": [
@@ -26,12 +53,25 @@ let project = Project(
             name: "BookmarkTests",
             destinations: .iOS,
             product: .unitTests,
-            bundleId: "dev.tuist.BookmarkTests",
+            bundleId: "com.wodnd.bookmark.tests",
+            deploymentTargets: .iOS("17.0"),
             infoPlist: .default,
             buildableFolders: [
                 "Bookmark/Tests"
             ],
-            dependencies: [.target(name: "Bookmark")]
+            dependencies: [.target(name: "Bookmark-Dev")]
         ),
+    ],
+    schemes: [
+        .scheme(
+            name: "Bookmark-Dev",
+            buildAction: .buildAction(targets: ["Bookmark-Dev"]),
+            runAction: .runAction(configuration: "Debug")
+        ),
+        .scheme(
+            name: "Bookmark-Live",
+            buildAction: .buildAction(targets: ["Bookmark-Live"]),
+            runAction: .runAction(configuration: "Release")
+        )
     ]
 )
